413501582d83999bb37131b68178af7fc544c27b
gautam <nathgoutam932gmail.com>
2021-10-09 23:16:09 +0530

    finished tutorial product

---
+++ src/components/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import Editor from "./Editor";
+import useLocalStorage from '../hooks/useLocalStorage';
+
+function App() {
+
+  const [html, setHtml] = useLocalStorage('html','')
+  const [css, setCss] = useLocalStorage('css','')
+  const [js, setJs] = useLocalStorage('js','')
+  const [srcDoc, setSrcDoc] = useState('')
+
+
+  useEffect(()=>{
+
+    const timeout = setTimeout(()=>{
+
+      setSrcDoc(`
+      <html>
+        <body>${html}<body/>
+        <style>${css}<style/>
+        <script>${js}<script/>
+      <html/>
+      `)
+
+      return () => clearTimeout(timeout)
+
+    },250)
+
+  },[html,css,js])
+
+  return (
+    <>
+      <div className="pane top">
+        <Editor 
+        languag="xml" 
+        displayName="HTML"
+        value={html}
+        onChange={setHtml}
+        />
+        <Editor 
+        languag="css" 
+        displayName="CSS"
+        value={css}
+        onChange={setCss}
+        />
+        <Editor 
+        languag="javascript" 
+        displayName="js"
+        value={js}
+        onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+        srcDoc={srcDoc} 
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        />
+      </div>
+
+    </>
+  )
+}
+
+export default App;
